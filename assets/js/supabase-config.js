5117c2c589d0b2cf7598f14001274368df63af5c
CrowRules Entertainment <crowrulesentertainment@gmail.com>
2026-09-06 14:10:14 -0700

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -9,7 +9,7 @@
 */
 
 window.CROW_SUPABASE_URL =
-  "https://zauxdqyssratvzmomozf.supabase.co/rest/v1/";
+  "https://zauxdqyssratvzmomozf.supabase.co";
 
 window.CROW_SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_-m6NuVfbrcs84OH7aGPrDw_AuLfXo8J";
